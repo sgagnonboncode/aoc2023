b3907e5e84c569d5b369c4bba8df35db19d3353c
Sébastien Gagnon <sebastien.gagnon2@norda.com>
2023-11-30 23:30:31 -0500

    setup solution runner

---
+++ src/solvers/day01.py
@@ -0,0 +1,9 @@
+from common.file_helpers import read_lines
+
+
+def solve_part1() -> int:
+    read_lines("input/day01/part1.txt")
+
+
+def solve_part2() -> int:
+    read_lines("input/day01/part2.txt")
