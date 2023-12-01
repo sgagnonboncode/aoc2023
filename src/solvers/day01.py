8e969147ce17a107f9f2d230de1dcb4daf2e383f
Sébastien Gagnon <sebastien.gagnon2@norda.com>
2023-12-01 00:19:28 -0500

    solve day 1

--- src/solvers/day01.py
+++ src/solvers/day01.py
@@ -1,9 +1,55 @@
 from common.file_helpers import read_lines
 
 
+def scan_words(line) -> str:
+    # insert numbers when words are found
+    # for example: zoneight234 becomes z1on8eight234
+
+    substitutions = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+
+    processed = ""
+    for i in range(0, len(line)):
+        forward = line[i:]
+
+        for word, number in substitutions.items():
+            if forward.startswith(word):
+                processed += number
+                break
+
+        processed += line[i]
+
+    return processed
+
+
+def filter_digits(line) -> int:
+    "filter out non-digit characters"
+    return "".join(filter(str.isdigit, line))
+
+
+def extract_calibration_value(line) -> int:
+    "extract calibration value from line"
+    digits = filter_digits(line)
+    return 10 * int(digits[0]) + int(digits[-1])
+
+
 def solve_part1() -> int:
-    read_lines("input/day01/part1.txt")
+    lines = read_lines("input/day01/part1.txt")
+    calibration_values = [extract_calibration_value(line) for line in lines]
+    return sum(calibration_values)
 
 
 def solve_part2() -> int:
-    read_lines("input/day01/part2.txt")
+    lines = read_lines("input/day01/part1.txt")
+    word_replace = [scan_words(line) for line in lines]
+    calibration_values = [extract_calibration_value(line) for line in word_replace]
+    return sum(calibration_values)
